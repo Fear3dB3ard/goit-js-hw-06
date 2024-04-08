4dfe929035f56ce9f3f6841c763965b0483933d5
Fear3dB3ard <craciunstefanmarian@yahoo.com>
2024-04-08 08:54:05 +0300

    Update task-01.js

--- JS/task-01.js
+++ JS/task-01.js
@@ -7,7 +7,7 @@ categoryItems.forEach(function (category) {
   const categoryTitle = category.querySelector("h2").textContent;
   const categoryElements = category.querySelectorAll("li").length;
 
-  
   console.log("Category: " + categoryTitle);
   console.log("Elements: " + categoryElements);
 });
+j;
