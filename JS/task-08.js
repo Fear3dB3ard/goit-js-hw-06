7e9937b932711e0e7ebc71c5136e91b183d81123
Fear3dB3ard <craciunstefanmarian@yahoo.com>
2024-04-09 18:54:12 +0300

    update task 8  with alert

--- JS/task-08.js
+++ JS/task-08.js
@@ -4,6 +4,17 @@ loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formData = new FormData(loginForm);
+  let isEmpty = false;
+  formData.forEach((value) => {
+    if (!value.trim()) {
+      isEmpty = true;
+    }
+  });
+
+  if (isEmpty) {
+    alert("Please fill in all the fields.");
+    return;
+  }
   const formValues = {};
   formData.forEach((value, key) => {
     formValues[key] = value;
